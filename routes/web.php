7b2ba64dd56f5620385e3acb7463dc1e74ce8d96
Almudena Calderon <1797322@alu.murciaeduca.es>
2023-01-09 19:59:41 +0100

    Tras aplicar php artisan breeze:install api

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
-use App\Http\Controllers\LugaresController;
-use App\Http\Controllers\HomeController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,26 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [HomeController::class, 'getHome']);
-
-Route::get('/lugares', [LugaresController::class, 'getIndex']);
-
-Route::get('/lugares/show/{id}', [LugaresController::class, 'getShow'])->middleware(['auth']);
-
-Route::get('/lugares/create', [LugaresController::class, 'getCreate'])->middleware(['auth']);
-
-Route::post('/lugares/create', [LugaresController::class, 'store']);
-
-Route::put('/lugares/edit/{id}', [LugaresController::class, 'putStore']);
-
-Route::get('/lugares/edit/{id}', [LugaresController::class, 'getEdit'])->middleware(['auth']);
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
